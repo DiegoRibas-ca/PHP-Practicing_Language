6d3faec818bedbc1cb5a77e753ffaa14a7aed333
DiegoRibas-ca <diegoribas.ca@gmail.com>
2018-06-04 18:55:21 -0400

    methods, properties and variables

--- Book.php
+++ Book.php
@@ -5,6 +5,22 @@ class Book
     public $title;
     public $author;
     public $available;
+
+    public function getPrintableTible() {
+        $result = $this->title . ' by ' . $this->author;
+        if (!$this->available) {
+            $result .= ' Not Available';
+        }
+        return $result;
+    }
+    public function getCopy() {
+        if($this->available < 1){
+            return false;
+        } else {
+            $this->available--;
+            return true;
+        }
+    }
 }
 
 $harry_potter = new Book();
@@ -12,6 +28,13 @@ $harry_potter->isbn = 989437943894;
 $harry_potter->title = "Harry Potter and the Prisioner of Askabam"; 
 $harry_potter->author = "JK"; 
 $harry_potter->available = 10; 
+
+if($harry_potter->getCopy()) {
+    echo 'Here is your copy of ' . $harry_potter->title. '<br>';
+} else {
+    echo 'Sorry its gone!';
+}
+
 var_dump($harry_potter);
 
 ?>
