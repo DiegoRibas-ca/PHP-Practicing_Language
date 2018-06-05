ca7651902dab7e1fa5d696ce7f9f2064a7ee3a18
DiegoRibas-ca <diegoribas.ca@gmail.com>
2018-06-04 20:51:13 -0400

    static properties

--- Customer.php
+++ Customer.php
@@ -1,12 +1,23 @@
 <?php
 
 class Customer {
+
+    private static $lastId = 0;
+
     private $id;
     private $firstname;
     private $surname;
     private $email;
 
     public function __construct($id, $firstname, $surname, $email) {
+        if($id == null) {
+            $this->id = ++self::$lastId;
+        } else {
+            $this->id = $id;
+            if($id > self::$lastId) {
+                self::$lastId = $id;
+            }
+        }
         $this->id = $id;
         $this->firstname = $firstname;
         $this->surname = $surname;
@@ -28,5 +39,9 @@ class Customer {
     }
     public function getEmail() {
         return $this->email;
+    }
+
+    public static function getLastId() {
+        return self::$lastId;
     }
 }
--- init.php
+++ init.php
@@ -6,8 +6,14 @@ require_once __DIR__ . '/Customer.php';
 $harry_potter = new Book(989437943894, "Harry Potter and the Prisioner of Askabam", "JK", 10);
 
 $first_customer = new Customer(1, "Diego", "Ribas", "diego@gmail.com");
+$first_customer = new Customer(null, "Simon", "Ribas", "Simon@gmail.com");
+$first_customer = new Customer(5, "Thiago", "Ribas", "thiago@gmail.com");
 
 echo $harry_potter;
 
 var_dump($first_customer->getFullName());
 var_dump($first_customer->setEmail('diegoribas@gmail.com'));
+
+$first_customer::getLastId();
+
+Customer::getLastId();
